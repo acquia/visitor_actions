476beac030103276fbae8cf86af8cbab86418f1a
Lisa Backer <lisa.backer@acquia.com>
2014-07-23 14:27:23 -0400

    Refactor to generic dialog window

--- modules/visitor_actions_ui/js/visitor_actions_ui.editor.js
+++ modules/visitor_actions_ui/js/visitor_actions_ui.editor.js
@@ -1,7 +1,7 @@
 /**
  * @file visitor_actions_ui.editor.js
  */
-(function (Drupal, $, _, Backbone) {
+(function (Drupal, $, _, Backbone, Dialog) {
 
 "use strict";
 
@@ -344,24 +344,15 @@ Drupal.visitorActions.ui = {
   /**
    * The model for an actionable element.
    */
-  ActionModel: Backbone.Model.extend({
-    defaults: {
-      // True if the Action element is available for activation.
-      enabled: false,
-      // True if the Action element is currently being configured.
-      active: false,
-      // A type of element, e.g. 'link', or 'form'.
-      type: null,
-      // A query selector to find this item.
-      selector: null
-    },
-
-    /**
-     * {@inheritdoc}
-     */
-    destroy: function (options) {
-      this.trigger('destroy', this, this.collection, options);
-    }
+  ActionModel: Dialog.models.DialogModel.extend({
+    defaults: _.extend({}, Dialog.models.DialogModel.prototype.defaults,
+      {
+        // True if the Action element is available for activation.
+        enabled: false,
+        // A type of element, e.g. 'link', or 'form'.
+        type: null
+      }
+    )
   }),
 
   /**
@@ -419,18 +410,10 @@ Drupal.visitorActions.ui = {
   /**
    * Backbone view for the dialog element that contains the interaction UI.
    */
-  ActionDialogVisualView: Backbone.View.extend({
+  ActionDialogVisualView: Dialog.views.ElementDialogView.extend({
 
-    /**
-     * {@inheritdoc}
-     */
-    initialize: function (options) {
-      // Save the actionable element as the anchor for this view.
-      this.anchor = this.el;
-
-      this.model.on('change:active', this.render, this);
-      this.model.on('change:active', this.deactivate, this);
-      this.model.on('destroy', this.remove, this);
+    initialize: function() {
+      this.parent('inherit', this.options);
     },
 
     /**
@@ -442,28 +425,11 @@ Drupal.visitorActions.ui = {
       }
       var that = this;
       var type = this.model.get('type');
-      // A bound function to trigger this' position method.
-      var posFn = $.proxy(this.position, this);
-      // Set the new dialog fragment as the el of this view.
-      this.setElement($(Drupal.theme('visitorActionsUIAssociativeDialog', {
-          id: this.model.id
-        }))
-        .appendTo('body'));
-      // Reposition the dialog on window scroll and resize.
-      $(window).off('.visitorActionsUI.actionDialogVisualView');
-      $(window).on('resize.visitorActionsUI.actionDialogVisualView scroll.visitorActionsUI.actionDialogVisualView', function (event) {
-        that.position();
-      });
-
-      /**
-       * Dismisses this instance of ActionDialogVisualView view.
-       */
-      Drupal.ajax.prototype.commands.visitor_actions_ui_dismiss = function (ajax, response, status) {
-        Drupal.ajax.prototype.commands.visitor_actions_ui_dismiss = null;
-        // Clean up the Drupal.ajax object reference for this element.
-        Drupal.ajax[that.anchor.id] = null;
-        that.model.set('active', false);
-      };
+      var formPath = Drupal.settings.basePath +
+        'visitor_actions/add/' +
+        Drupal.encodePath(type) +
+        '?path=' + Drupal.encodePath(Drupal.settings.visitor_actions.currentPath);
+      model.set('formPath', formPath);
 
       /**
        * Redirects to the callback that disables edit mode.
@@ -525,100 +491,7 @@ Drupal.visitorActions.ui = {
         $dialog.find('[name="identifier[' + type + ']"]').val(Drupal.formatString(selector));
       };
 
-      /**
-       * Override the Drupal.ajax error handler for the form redirection error.
-       *
-       * Remove the alert() call.
-       */
-      var ajaxError = Drupal.ajax.prototype.error;
-      Drupal.ajax.prototype.error = function (response, uri) {
-        // Remove the progress element.
-        if (this.progress.element) {
-          $(this.progress.element).remove();
-        }
-        if (this.progress.object) {
-          this.progress.object.stopMonitoring();
-        }
-        // Undo hide.
-        $(this.wrapper).show();
-        // Re-enable the element.
-        $(this.element).removeClass('progress-disabled').removeAttr('disabled');
-        // Reattach behaviors, if they were detached in beforeSerialize().
-        if (this.form) {
-          var settings = response.settings || this.settings || Drupal.settings;
-          Drupal.attachBehaviors(this.form, settings);
-        }
-      };
-
-      // We need to know when the insert command is called and position the
-      // dialog after the form DOM elements have been inserted.
-      var insert = Drupal.ajax.prototype.commands.insert;
-      /**
-       * Hooks into the Drupal.ajax insert command.
-       */
-      Drupal.ajax.prototype.commands.insert = function (ajax, response, status) {
-        // Deal with incremented form IDs.
-        if (ajax.wrapper === '#visitor-actions-form') {
-          ajax.wrapper = '#' + ajax.form[0].id;
-        }
-        // Call the original insert command.
-        insert.call(this, ajax, response, status);
-        // Call the position method.
-        if (ajax.event === 'newAction.visitorActionsUI') {
-          that.position(function () {
-            that.show();
-          });
-        }
-        // Put the original insert command back.
-        Drupal.ajax.prototype.commands.insert = insert;
-      }
-
-      // Perform an AJAX request to get the add action form.
-      Drupal.ajax[this.anchor.id] = new Drupal.ajax(this.anchor.id, this.anchor, {
-        url: Drupal.settings.basePath +
-          'visitor_actions/add/' +
-          Drupal.encodePath(type) +
-          '?path=' + Drupal.encodePath(Drupal.settings.visitor_actions.currentPath),
-        event: 'newAction.visitorActionsUI',
-        wrapper: that.model.id + '-dialog .visitor-actions-ui-placeholder',
-        progress: {
-          type: null
-        },
-        success: function (response, status) {
-          $('#' + that.anchor.id).off('newAction.visitorActionsUI');
-          Drupal.ajax.prototype.success.call(this, response, status);
-        },
-        complete: function () {
-          // Put the original Drupal.ajax error handler back.
-          Drupal.ajax.prototype.error = ajaxError;
-          ajaxError = null;
-        }
-      });
-
-      // Trigger the form load after the dialog has rendered.
-      $('#' + this.anchor.id).trigger('newAction.visitorActionsUI');
-    },
-
-    /**
-     * Removes the form dialog from the DOM.
-     *
-     * @param Backbone.Model model
-     * @param Boolean active
-     */
-    deactivate: function (model, active) {
-      if (active) {
-        return;
-      }
-      this.el.parentNode.removeChild(this.el);
-    },
-
-    /**
-     * {@inheritdoc}
-     */
-    remove: function (model) {
-      $(window).off('.visitorActionsUI.actionDialogVisualView');
-      this.setElement(null);
-      Backbone.View.prototype.remove.call(this);
+      this.parent('render', model, active);
     },
 
     /**
@@ -628,110 +501,7 @@ Drupal.visitorActions.ui = {
      *   (optional) A function to invoke after positioning has finished.
      */
     position: function (callback) {
-      clearTimeout(this.timer);
-
-      var that = this;
-      // Vary the edge of the positioning according to the direction of language
-      // in the document.
-      var edge = (document.documentElement.dir === 'rtl') ? 'right' : 'left';
-      // A time unit to wait until the entity toolbar is repositioned.
-      var delay = 100;
-      // Alighn the dialog with the edge of the highlighted element outline.
-      var horizontalPadding = -4;
-
-      /**
-       * Refines the positioning algorithm of jquery.ui.position().
-       *
-       * Invoked as the 'using' callback of jquery.ui.position() in
-       * positionToolbar().
-       *
-       * @param Object suggested
-       *   A hash of top and left values for the position that should be set. It
-       *   can be forwarded to .css() or .animate().
-       * @param Object info
-       *   The position and dimensions of both the 'my' element and the 'of'
-       *   elements, as well as calculations to their relative position. This
-       *   object contains the following properties:
-       *     - Object element: A hash that contains information about the HTML
-       *     element that will be positioned. Also known as the 'my' element.
-       *     - Object target: A hash that contains information about the HTML
-       *     element that the 'my' element will be positioned against. Also known
-       *     as the 'of' element.
-       */
-      function refinePosition (suggested, info) {
-        var $pointer = info.element.element.find('.visitor-actions-ui-dialog-pointer');
-        var coords = {
-          left: Math.floor(suggested.left),
-          top: Math.floor(suggested.top)
-        };
-
-        /**
-         * Calculates the position of the pointer in relation to the target.
-         */
-        function pointerPosition () {
-          var swag = info.target.left - info.element.left;
-          var element = info.element.element;
-          var elWidth = element.outerWidth();
-          var pointerWidth = $pointer.outerWidth();
-          var gutter = parseInt(element.css('padding-left').slice(0, -2), 10) + parseInt(element.css('padding-right').slice(0, -2), 10);
-          // Don't let the value be less than zero.
-          swag = (swag > gutter) ? swag : gutter;
-          // Don't let the value be greater than the width of the element.
-          swag = (swag + pointerWidth < (elWidth - gutter)) ? swag : elWidth - pointerWidth - gutter;
-          return swag;
-        }
-
-        // Determine if the pointer should be on the top or bottom.
-        info.element.element.toggleClass('visitor-actions-ui-dialog-pointer-top', info.vertical === 'top');
-        // Determine if the pointer should be on the left or right.
-        $pointer.css('left', pointerPosition());
-        // Apply the positioning.
-        info.element.element.css(coords);
-      }
-
-      /**
-       * Calls the jquery.ui.position() method on the $el of this view.
-       *
-       * @param function callback
-       *   (optional) A function to invoke after positioning has finished.
-       */
-      function positionToolbar (callback) {
-        that.$el
-          .position_visitor_actions_ui({
-            my: edge + ' bottom',
-            // Move the toolbar 1px towards the start edge of the 'of' element,
-            // plus any horizontal padding that may have been added to the element
-            // that is being added, to prevent unwanted horizontal movement.
-            at: edge + '+' + (1 + horizontalPadding) + ' top',
-            of: that.anchor,
-            collision: 'flipfit',
-            using: refinePosition
-          });
-        // Invoke an optional callback after the positioning has finished.
-        if (callback) {
-          callback();
-        }
-      }
-
-      // Uses the jQuery.ui.position() method. Use a timeout to move the toolbar
-      // only after the user has focused on an editable for 250ms. This prevents
-      // the toolbar from jumping around the screen.
-      this.timer = setTimeout(function () {
-        // Render the position in the next execution cycle, so that animations on
-        // the field have time to process. This is not strictly speaking, a
-        // guarantee that all animations will be finished, but it's a simple way
-        // to get better positioning without too much additional code.
-        _.defer(positionToolbar, callback);
-      }, delay);
-    },
-
-    /**
-     * Reveals the add action dialog after it has been positioned.
-     *
-     * Called as part of the render sequence.
-     */
-    show: function () {
-      this.el.style.display = 'block';
+      this.parent('position', callback, 100);
     },
 
     /**
@@ -922,26 +692,6 @@ Drupal.visitorActions.ui = {
 };
 
 /**
- * Theme function for an associative dialog.
- *
- * @param Object options
- *   Contains the following key:
- *   - id: The id associated with the actionable element.
- *
- * @return String
- *   The corresponding HTML.
- */
-Drupal.theme.visitorActionsUIAssociativeDialog = function (options) {
-  var html = '';
-  html += '<div id="' + options.id + '-dialog" class="visitor-actions-ui-dialog clearfix" style="display:none;">';
-  html += '<i class="visitor-actions-ui-dialog-pointer"></i>';
-  html += '<div class="visitor-actions-ui-dialog-content">';
-  html += '<div class="visitor-actions-ui-placeholder"></div>';
-  html += '</div>';
-  return html;
-};
-
-/**
  * Theme function for a container to present actionable elements without IDs.
  *
  * @param Object options
@@ -969,4 +719,4 @@ Drupal.theme.visitorActionsUIButton = function (options) {
   return '<button>' + options.text + '</button>';
 };
 
-}(Drupal, jQuery, _, Backbone));
+}(Drupal, jQuery, _, Backbone, Drupal.visitorActions.ui.dialog));

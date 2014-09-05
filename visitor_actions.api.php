6addb92a074af27bac9be8d10941f46561170ac3
Lisa Backer <lisa.backer@acquia.com>
2014-09-05 11:33:04 -0400

    Moved selector hook into visitor_actions_ui

--- visitor_actions.api.php
+++ visitor_actions.api.php
@@ -96,28 +96,6 @@ function hook_visitor_actions_delete_action() {
 }
 
 /**
- * Provides a listing of JavaScript regular expressions to be ignored when selecting
- * elements and/or generating an element selector.
- *
- * Returns an array with two keys:
- * - classes: An array of regular expressions to ignore in classes.  This should
- *   match the entire class name.
- * - ids: An array of regular expressions to test for ids.  Note that this only
- *   needs to match any part of the ID name rather than the entire name.
- */
-function hook_visitor_actions_selector_ignore() {
-  return array(
-    'classes' => array(
-      'someclass',
-      '[a-zA-Z0-9]-processed',
-    ),
-    'ids' => array(
-      'visitorActions-[a-zA-Z0-9\_]',
-    ),
-  );
-}
-
-/**
  * React to client-side actions being loaded on the page.
  *
  * @param $page
